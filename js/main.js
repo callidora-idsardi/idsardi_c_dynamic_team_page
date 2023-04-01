4f5584e2736e74ed778f2be6853b4a657685e99f
callidora-partner <idscallie@gmail.com>
2023-04-01 15:51:00 -0400

    created dynamic content and added Magnus' bio information

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
+console.log('js is wired!');
+
+let theButtons = document.querySelectorAll('button'),
+    infoDiv = document.querySelector('#info-div');
+
+
+let members = {
+    calli : {
+		Name: 'Callidora Idsardi',
+		Bio: 'Calli Info',
+		Image:
+
+    },
+
+    magnus : {
+       Name: 'Magnus Bane',
+	   Bio: 'Magnus is an essential team member here at Squeezit. As our in-house dog, he provides daily happiness for our team and clients. Without Magnus, we would not be happy.' 
+	   Image: 
+    },  
+};
+
+// functions go here
+function listInfo() {
+    // empty out the list
+    infoDiv.innerHTML = "";
+    
+    // and then repopulate it with the correct data
+    for (item in members[this.dataset.team]) {
+        let newItem = document.createElement('p');
+
+        newItem.textContent = members[this.dataset.team][item];
+        infoDiv.appendChild(newItem);
+    }    
+
+
+}
+
+
+
+// add event handling here
+theButtons.forEach(teamButton => teamButton.addEventListener('click', listInfo)),
+
+
+
+
+
+
+
+
+
+
+
+
+
+// HAMBURGER MENU
+
 (function(){
 	"use strict";	
 	console.log("fired");
